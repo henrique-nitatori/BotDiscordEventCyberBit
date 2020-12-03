5e9902dec4f7d55e45ff6368d9df5aaa22e4f12d
Gabriel Eich <eich.gabriel1@gmail.com>
2020-12-02 21:49:20 -0300

    prefix config

--- src/BotStart.js
+++ src/BotStart.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const { token } = require('../bot.config.json')
+const { prefix } = require('../config.json')
 const client = new Discord.Client();
-
 //Mensagem quando um boot entrar no servidor
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,9 +9,11 @@ client.on('ready', () => {
 
 //Quando for digitado no chat 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+  console.log(prefix)
+  console.log(msg.content)
+  if (!msg.content.startsWith(prefix)) return;
+
+  msg.reply('pong');
 });
 
 //Message quando o bot disconectar
