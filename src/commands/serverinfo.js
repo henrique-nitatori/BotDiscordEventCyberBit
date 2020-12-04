d0998d04a349000cf2b770ab32ee0e94f91321c7
Henrique nitatori <henrique_nitatori@hotmail.com>
2020-12-03 21:49:45 -0300

    added command serverinfo

--- src/commands/serverinfo.js
+++ src/commands/serverinfo.js
@@ -1,20 +1,33 @@
 const Discord = require('discord.js');
 
-
 function serverInfo(msg) {
     const date = new Date(msg.guild.createdAt)
+    const createdAt = new Date(msg.guild.createdTimestamp)
     const month = date.toLocaleDateString('en-US', {dateStyle: 'medium'}).split(' ')[0]
-    console.log(msg.guild.createdTimestamp)
-    const dateString = `${date.getDay()} de ${month} ás, ${date.getHours()}:${date.getMinutes()} ()`
+    
+    const dateString = `${date.getDay()} de ${month} ás, ${date.getHours()}:${date.getMinutes()}
+    (${createdAt.getHours()} horas ${createdAt.getMinutes()} minutos 
+    ${createdAt.getSeconds()} segundos) `
+
+    const memberOfTheGuild = msg.guild.memberCount
+    let textChannelNumber = 0
+    let voiceChannelNumber = 0
+
+    msg.channel.guild.channels.cache.forEach(value => {
+        if(Object(value).type === 'text') textChannelNumber += 1;
+        if(Object(value).type === 'voice') voiceChannelNumber += 1;
+    })
+    
     embed = new Discord.MessageEmbed()
     .setColor("#5c0fd9")
-    .setTitle("Help")
+    .setTitle("Samuel hackiou essas infos :eyes: ")
     .addFields(
-        { name: ':computer: ID', value: 'Eich#8617', inline: true },
-        { name: ':crown: Dono', value: '539262905980551179', inline: true },
-        { name: ':earth_americas: Região', value: '1 ano 10 meses 5 dias', inline: true },
-        { name: ':speech_balloon: Canais (3)', value: ':pencil: **Texto**: 2 \n :speaking_head: **Voz**: 1 ', inline: false },
-        { name: ':date: Criado em', value: `${dateString}`, inline: false },
+        { name: ':computer: ID', value: `${msg.guild.id}`, inline: true },
+        { name: ':crown: Dono', value: `**${msg.guild.owner.displayName}**`, inline: true },
+        { name: ':earth_americas: Região', value: `${msg.guild.region}`, inline: true },
+        { name: `:speech_balloon: Canais (${textChannelNumber + voiceChannelNumber})`, value: `:pencil: **Texto**: ${textChannelNumber} \n :speaking_head: **Voz**: ${voiceChannelNumber} \n`, inline: true },
+        { name: ':date: Criado em', value: `${dateString}`, inline: true },
+        { name: ':busts_in_silhouette: Membros ', value: `${memberOfTheGuild}`, inline: true },
     )
 
     msg.channel.send(embed)
